e84882da854262f357100b0b2f2ed7cb35f9a8cc
xVanTuring <xVanTuring@outlook.com>
2020-01-13 19:24:27 -0600

    fix `__static` variable

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -25,14 +25,6 @@ if (!isPythonInstalled) {
   // require('./python').init()
 }
 
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
-}
-
 // 未捕获的rejections
 process.on('unhandledRejection', (reason, p) => {
   logger.error(`Unhandled Rejection at: Promise ${p}, reason: ${reason}`)
